ae6a06381dd4b97ff3338ad565b2eb1b4a88d46e
ashok <k.ashok502@gmail.com>
2018-10-29 21:56:24 +0530

    import gmail and yahoo contacts

--- config/initializers/omnicontacts.rb
+++ config/initializers/omnicontacts.rb
@@ -1,5 +1,5 @@
 require "omnicontacts"
 Rails.application.middleware.use OmniContacts::Builder do
-  importer :gmail, "399845174723-200dcmtl85b06n1dmb4cbh1vmkcjmbeg.apps.googleusercontent.com", "r5rniUt3Sj_b-TU3C9dPAe8q", {:redirect_path => "/contacts/gmail/contact_callback"}
-  # importer :yahoo, "consumer_id", "consumer_secret", {:callback_path => "/callback"}
+  importer :gmail, ENV["GID"], ENV["GSECRET"], {:redirect_path => "/contacts/gmail/contact_callback"}
+  importer :yahoo, "consumer_id", "consumer_secret", {:callback_path => "/callback"}
 end
