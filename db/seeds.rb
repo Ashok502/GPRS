d9e7b964f87e777eba7d1e5a17b01f3997418570
Ashok <Ashok502@users.noreply.github.com>
2018-11-08 14:44:27 +0530

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-User.create(email: 'admin@gmail.com', password: '12345678', password_confirmation: '12345678', username: 'admin', role: 'admin')
-User.create(email: 'ashok@gmail.com', password: '12345678', password_confirmation: '12345678', username: 'ashok')
-User.create(email: 'kapil@gmail.com', password: '12345678', password_confirmation: '12345678', username: 'kapil')
-User.create(email: 'sunil@gmail.com', password: '12345678', password_confirmation: '12345678', username: 'sunil')
