e8eb3f0dcd3586d5a9761d506b55e05912cbbb3b
Ashok <Ashok502@users.noreply.github.com>
2018-11-12 15:32:51 +0530

    Update message_relay_job.rb

--- app/jobs/message_relay_job.rb
+++ app/jobs/message_relay_job.rb
@@ -6,7 +6,9 @@ class MessageRelayJob < ApplicationJob
 			username: message.user.username,
 			body: message.body,
 			chatroom_id: message.chatroom.id,
-			date: message.created_at.strftime("%d %b. %Y")
+			date: message.created_at.strftime("%d %b. %Y"),
+			current_user_id: message.chatroom.user_id,
+			user_id: message.user_id
 		}
 	end
 end
