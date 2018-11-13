33ffb236dc852c48ae2cc3d97c7730a3d4b81d2d
Ashok <Ashok502@users.noreply.github.com>
2018-11-13 13:27:32 +0530

    Update message_relay_job.rb

--- app/jobs/message_relay_job.rb
+++ app/jobs/message_relay_job.rb
@@ -1,13 +1,13 @@
 class MessageRelayJob < ApplicationJob
 	queue_as :default
 
-	def perform(message)
+	def perform(message, user)
 		ActionCable.server.broadcast "chatrooms:#{message.chatroom.id}", {
 			username: message.user.username,
 			body: message.body,
 			chatroom_id: message.chatroom.id,
 			date: message.created_at.strftime("%d %b. %Y"),
-			current_user_id: message.chatroom.user_id,
+			current_user_id: user.id,
 			user_id: message.user_id
 		}
 	end
