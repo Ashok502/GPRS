9b6a7dc4845adadb98092cc94871d541366f3df4
ashok <k.ashok502@gmail.com>
2019-11-14 09:33:18 +0530

    admin issue fixed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -27,6 +27,6 @@ module ApplicationHelper
 	end
 	
 	def google_map(center)
-          "https://maps.googleapis.com/maps/api/staticmap?center=#{center}&size=300x300&zoom=17"
-        end
+    "https://maps.googleapis.com/maps/api/staticmap?center=#{center}&size=300x300&zoom=17"
+  end
 end
